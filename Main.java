c31c8c9e0a37a67814e87dd164f0fba9d6fcc427
Danylo-S1502 <dsukhom1@stu.vistula.edu.pl>
2025-12-18 10:30:25 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // create simulator object
+        TrafficLightSimulator simulator =
+                new TrafficLightSimulator(TrafficLightColor.RED);
+
+        // loop limiting number of executions
+        for (int i = 0; i < 6; i++) {
+            System.out.println(simulator.toString());
+            simulator.changeColor(simulator.getCurrentColor());
+        }
+    }
+}
